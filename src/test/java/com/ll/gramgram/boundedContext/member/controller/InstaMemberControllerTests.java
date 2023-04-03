f006b2bfcd78f2ed96a416176e4233b4159a01d3
hanbonghun <bng4535@khu.ac.kr>
2023-04-03 16:16:39 +0900

    입력 폼 처리 시작

--- src/test/java/com/ll/gramgram/boundedContext/member/controller/InstaMemberControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/InstaMemberControllerTests.java
@@ -1,6 +1,7 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
 
+import com.ll.gramgram.boundedContext.member.entity.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -67,5 +71,26 @@ public class InstaMemberControllerTests {
                 .andExpect(handler().methodName("showConnect"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlPattern("**/member/login**"));
+    }
+
+    @Test
+    @DisplayName("인스타회원 정보 입력 폼 처리")
+    @WithUserDetails("user1")
+    void t003() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/instaMember/connect")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "abc123")
+                        .param("gender", "W")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(InstaMemberController.class))
+                .andExpect(handler().methodName("connect"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("/pop**"));
     }
 }
